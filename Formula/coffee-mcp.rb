d8815972600ad65bc0dcdad8d923b130d4e6d004
Michael Critz <github@michaelcritz.com>
2026-06-20 11:12:20 -0700

    homebrew versiom bump

--- Formula/coffee-mcp.rb
+++ Formula/coffee-mcp.rb
@@ -4,7 +4,7 @@ class CoffeeMcp < Formula
   url "https://github.com/coffeecoffeecoffeecoffee/CoffeeMCP/releases/download/v0.1.1/CoffeeMCP-v0.1.1-macos.tar.gz"
   sha256 "ae28826df65bb5fbab8d4fe66f0ed363f464f66d1087dc16efeeaa32393667f2"
   license "MIT"
-  version "0.1.1"
+  version "1.2.1"
 
   depends_on :macos => :ventura
 
